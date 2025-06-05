ae43b6405504ab611d51e79b3ed2547c72853938
Nick Kleiner <nkleiner@dcscorp.com>
2025-06-05 08:45:36 -0400

    Gave the print planner its own implementation to protect against the changing message scheme of the AresPlanner. Set version to 1.0.0

---
+++ PythonAres/AresPlanning/print_planner.py
@@ -0,0 +1,39 @@
+from concurrent import futures
+from .messages import print_planner_pb2_grpc
+from .messages import print_planner_pb2
+
+import grpc
+
+class AresPlanner(print_planner_pb2_grpc.PrintPlannerGrpc):
+    def __init__(self, port):
+        self.port = port
+        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        print_planner_pb2_grpc.add_PrintPlannerGrpcServicer_to_server(self, self.server)
+        self.server.add_insecure_port(f"localhost:{port}")
+
+    def start(self):
+        if self.server:
+            self.server.start()
+            print(f"Planner started, listening on {self.port}")
+            self.server.wait_for_termination()        
+
+    def stop(self):
+        if self.server:
+            self.server.stop()
+            print("Planner successfully stopped.")
+
+    def Plan(self, request, context):
+        #Can't override this method, as it's bound to the receival of our request.
+        #Call the user method which theoretically has been overridden.
+        return self.DoPlanning(request, context)
+        #Run Planning Service
+
+    def DoPlanning(request, context):
+        print("Received a planning request, but no override for plan logic is in place!")
+        print("To utilize custom planning logic, override the DoPlanning method of the AresPlanner class.")
+        print("Returning an empty plan response.")
+        return print_planner_pb2.PlanResponse()
+
+    def __del__(self):
+        #Stop the gRPC server when the object is disposed
+        self.stop() 
