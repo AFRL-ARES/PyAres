9f301fade04245fa95c0952e6875449058bee5ad
Nick Kleiner <nkleiner@dcscorp.com>
2025-07-09 08:12:34 -0400

    A few minor adjustments to the print planner

--- PyAres/Planning/__init__.py
+++ PyAres/Planning/__init__.py
@@ -1,9 +1,11 @@
 from .models import PlanningParameter, PlanRequest, PlanResponse
 from .service import AresPlannerService
+from .print_planner import PrintPlanner
 
 __all__ = [
     "PlanningParameter",
     "PlanRequest",
     "PlanResponse",
-    "AresPlannerService"
+    "AresPlannerService",
+    "PrintPlanner"
 ]
--- PyAres/Planning/print_planner.py
+++ PyAres/Planning/print_planner.py
@@ -4,7 +4,7 @@ from .messages import print_planner_pb2
 
 import grpc
 
-class AresPlanner(print_planner_pb2_grpc.PrintPlannerGrpc):
+class PrintPlanner(print_planner_pb2_grpc.PrintPlannerGrpc):
     def __init__(self, port):
         self.port = port
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -22,13 +22,12 @@ class AresPlanner(print_planner_pb2_grpc.PrintPlannerGrpc):
             self.server.stop()
             print("Print planner successfully stopped.")
 
-    def Plan(self, request, context):
+    def Plan(self, request: print_planner_pb2.PrintPlanRequest, context) -> print_planner_pb2.PrintPlanResponse:
         #Can't override this method, as it's bound to the receival of our request.
         #Call the user method which theoretically has been overridden.
         return self.DoPlanning(request, context)
-        #Run Planning Service
 
-    def DoPlanning(request, context):
+    def DoPlanning(request: print_planner_pb2.PrintPlanRequest, context) -> print_planner_pb2.PrintPlanResponse:
         print("Received a print planning request, but no override for plan logic is in place!")
         print("To utilize custom planning logic, override the DoPlanning method of the AresPlanner class.")
         print("Returning an empty plan response.")
